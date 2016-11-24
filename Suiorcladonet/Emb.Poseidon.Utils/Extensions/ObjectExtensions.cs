c5534adb0a9ab970ed3054aae6b00a297a5bbb63
uialberto <uialberto@gmail.com>
2016-11-24 00:44:38 -0400

    Creacion de objetos de extension para string y object

---
+++ Suiorcladonet/Emb.Poseidon.Utils/Extensions/ObjectExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emb.Poseidon.Utils.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static string ToStringEmpty(this object obj)
+        {
+            return obj?.ToString() ?? string.Empty;
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static string ToStringEmpty(this string obj)
+        {
+            return obj ?? string.Empty;
+        }
+    }
+}
