e1d717db500f439b14201619f44bc8e6b5eb1374
uialberto <uialberto@gmail.com>
2016-11-23 23:49:43 -0400

    Creacion e inicializacion de proyectos

---
+++ Suiorcladonet/Emb.Poseidon.Oracle.DataAccess/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emb.Poseidon.Oracle.DataAccess
+{
+    public class Class1
+    {
+    }
+}
